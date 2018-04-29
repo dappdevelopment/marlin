2762aebd988eb85c9fed2e1ff36f3e3a44a05772
Pratyaksh Sharma <pratyaksh@me.com>
2018-04-29 13:26:20 -0700

    added tokenSupply, transfer, getBalance

--- contracts/marlin.sol
+++ contracts/marlin.sol
@@ -1,6 +1,9 @@
 // abstract idea for the contract
 contract Marlin {
 
+	uint256 totalSupply = 7.6e9;
+	event BalanceChanged(address indexed _address, uint256 _balance);
+
 	struct UrlStats {
 		uint256 replication; // number of peers serving this url
 		uint256 volume; // total volume of traffic (in bytes) served
@@ -17,24 +20,41 @@ contract Marlin {
 		string name; // used only for display purposes
 		string email; // used only for display purposes
 		uint256 creationTimestamp; // block timestamp of when createPublisher called
+		uint256 balance;
 		Url[] urls; // list of urls added by publisher
 	}
 
 	mapping (address => Publisher) private publishers;
 	
 	constructor() public {
-
+		createPublisher(msg.sender, "marlin-admin", "admin@marlin.pro");
+		publishers[msg.sender].balance = totalSupply;
 	}
 
 	function createPublisher(
-		address _addr, string _name, string _email) public returns (bool) {
+		address _addr, string _name, string _email) public returns (bool success) {
 		Publisher memory p = Publisher({
 			name: _name,
 			email: _email, 
 			creationTimestamp: block.timestamp,
+			balance: 0,
 			urls: new Url[](0)
 		});
 		publishers[_addr] = p;
-		return true;
+		success = true;
+	}
+
+	function transfer(address _to, uint256 _value) public returns (bool success) {
+		require(publishers[msg.sender].balance >= _value);
+		publishers[msg.sender].balance -= _value;
+        publishers[_to].balance += _value;
+        emit BalanceChanged(msg.sender, publishers[msg.sender].balance);
+        emit BalanceChanged(_to, publishers[_to].balance);
+        success = true;
+	}
+
+	function getBalance(address _addr) public returns (uint256) {
+		require(_addr == msg.sender);
+		return publishers[_addr].balance;
 	}
 }
