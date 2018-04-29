02c37e841e64714a2af4328debb4fcda889bb476
Josh-Payne <snare117@gmail.com>
2018-04-29 14:27:43 -0700

    truffle

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,7 @@
 function cdnIFY() { // gets text from text box input from CDN-ify
   var url = document.getElementById("url").value;
   console.log(url);
+  app()
   // add user, url to blockchain?
 }
 
@@ -66,3 +67,63 @@ function submit() { // gets text from text fields to sign up a user
   console.log(email)
   console.log(pw1)
 }
+
+function app() {
+  if (typeof web3 == 'undefined') throw 'No web3 detected. Is Metamask/Mist being used?';
+  web3 = new Web3(web3.currentProvider); // MetaMask injected Ethereum provider
+  console.log("Using web3 version: " + Web3.version);
+
+  var contract;
+  var userAccount;
+
+  var contractDataPromise = $.getJSON('Marlin.json');
+  var networkIdPromise = web3.eth.net.getId(); // resolves on the current network id
+  var accountsPromise = web3.eth.getAccounts(); // resolves on an array of accounts
+
+  Promise.all([contractDataPromise, networkIdPromise, accountsPromise])
+    .then(function initApp(results) {
+      var contractData = results[0];
+      var networkId = results[1];
+      var accounts = results[2];
+      userAccount = accounts[0];
+
+      if (!(networkId in contractData.networks)) {
+         throw new Error("Contract not found in selected Ethereum network on MetaMask.");
+      }
+
+
+      var contractAddress = contractData.networks[networkId].address;
+      contract = new web3.eth.Contract(contractData.abi, contractAddress);
+    })
+    .then(refreshBalance)
+     .catch(console.error);
+
+
+    function refreshBalance() { // Returns web3's PromiEvent
+       // Calling the contract (try with/without declaring view)
+       contract.methods.balanceOf(userAccount).call().then(function (balance) {
+         $('#display').text(balance + " CDT");
+         $("#loader").hide();
+       });
+     }
+    function transfer(to, amount) {
+    console.log(to, amount)
+    if (!to || !amount) return console.log("Fill in both fields");
+
+    $("#loader").show();
+
+    contract.methods.transfer(to, amount).send({from: userAccount})
+      .then(refreshBalance)
+      .catch(function (e) {
+        $("#loader").hide();
+      });
+  }
+
+  $("#button").click(function() {
+    var toAddress = $("#address").val();
+    var amount = $("#amount").val();
+    transfer(toAddress, amount);
+  });
+
+}
+$(document).ready(app);
